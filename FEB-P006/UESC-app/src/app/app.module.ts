10afc2cd02c0cf35f860290c56501b2d4cdcad0e
mfnswdev <engenharia.mfn@gmail.com>
2024-01-14 20:08:49 -0300

    Create UESC-App components

---
+++ FEB-P006/UESC-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { DestaqueComponent } from './destaque/destaque.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { ResultadosComponent } from './resultados/resultados.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NoticiasComponent,
+    DestaqueComponent,
+    ServicosComponent,
+    ResultadosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
